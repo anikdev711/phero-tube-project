ce773d8976d5e22d358ff03db08b065ae99a802d
anikdev711 <anikdev100@gmail.com>
2023-09-01 23:54:53 +0600

    category with no content added

--- js/phtube.js
+++ js/phtube.js
@@ -3,8 +3,11 @@ const handleMyPhTubeCategories = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
     // console.log(data.data);
+
     const myPhtubeTabContainer = document.getElementById('myphtube-tab-container');
+
     data.data.forEach((phtubeCategory) => {
+
         const div = document.createElement('div');
         div.innerHTML = `
             <a onclick="handleMyPhTubeElements('${phtubeCategory.category_id}')" class="tab tab-active">${phtubeCategory.category}</a>
@@ -14,30 +17,43 @@ const handleMyPhTubeCategories = async () => {
 };
 
 const handleMyPhTubeElements = async (phtubeCategoryId) => {
+
     // console.log(phtubeCategoryId);
+
+
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${phtubeCategoryId}`);
     const data = await response.json();
-    // console.log(data);
+
+    const noContentContainer = document.getElementById('no-content-container');
+
+    if(data.data.length === 0){
+        noContentContainer.classList.remove('hidden')
+    }
+    else{
+        noContentContainer.classList.add('hidden')
+    }
+    
+
     const myPhtubeCardContainer = document.getElementById('myphtube-card-container');
     myPhtubeCardContainer.innerHTML = "";
 
     data.data?.forEach((phtubeElements) => {
+        
         // console.log(phtubeElements.authors[0].verified);
-        const verificationBadge = phtubeElements.authors[0].verified;
+        const verificationBlueTickMark = phtubeElements.authors[0].verified;
 
-        console.log(phtubeElements.others.posted_date);
+        // console.log(phtubeElements.others.posted_date);
         let postDateSeconds = phtubeElements.others.posted_date;
         let postDateHours = Math.floor(postDateSeconds / 3600);
         let postDateMinutes = Math.floor((postDateSeconds % 3600) / 60);
         let timeInHoursMinutes = `${postDateHours > 0 ? postDateHours + ' ' + 'hrs' : ''} ${postDateMinutes > 0 ? postDateMinutes + ' ' + 'min' + ' ' + 'ago' : ''}`;
         // console.log(timeInHoursMinutes);
 
-
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="card w-72 h-96 bg-base-100 shadow-xl">
             <figure><img src=${phtubeElements?.thumbnail} alt="thumbnail-image" /></figure>
-            <p class="text-right -my-6 bg-[#171717] text-white p-2">${timeInHoursMinutes}</p>
+            <p class="text-right -my-4 bg-[#171717] text-white p-2">${timeInHoursMinutes}</p>
             <div class="card-body">
 
                 <div class="flex flex-row">
@@ -48,7 +64,7 @@ const handleMyPhTubeElements = async (phtubeCategoryId) => {
                 <div class="flex flex-row">
                     <p>${phtubeElements?.authors[0]?.profile_name}</p>
                     <div>
-                    ${verificationBadge === true ? '<img src="./images/bluetick.svg" alt="blue-tick"/>' : ''}
+                    ${verificationBlueTickMark === true ? '<img src="./images/bluetick.svg" alt="blue-tick"/>' : ''}
                     </div>
                 </div>
 
@@ -56,7 +72,9 @@ const handleMyPhTubeElements = async (phtubeCategoryId) => {
             
             </div>
         </div>
+            
         `;
+        
         myPhtubeCardContainer.appendChild(div);
 
     });
