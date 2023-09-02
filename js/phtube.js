af6c8a76ad1a8f6fdac09704c0ce31659e008026
anikdev711 <anikdev100@gmail.com>
2023-09-02 13:43:31 +0600

    sort added

--- js/phtube.js
+++ js/phtube.js
@@ -20,25 +20,24 @@ const handleMyPhTubeElements = async (phtubeCategoryId) => {
 
     // console.log(phtubeCategoryId);
 
-
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${phtubeCategoryId}`);
     const data = await response.json();
 
     const noContentContainer = document.getElementById('no-content-container');
 
-    if(data.data.length === 0){
+    if (data.data.length === 0) {
         noContentContainer.classList.remove('hidden')
     }
-    else{
+    else {
         noContentContainer.classList.add('hidden')
     }
-    
+
 
     const myPhtubeCardContainer = document.getElementById('myphtube-card-container');
     myPhtubeCardContainer.innerHTML = "";
 
     data.data?.forEach((phtubeElements) => {
-        
+
         // console.log(phtubeElements.authors[0].verified);
         const verificationBlueTickMark = phtubeElements.authors[0].verified;
 
@@ -51,7 +50,7 @@ const handleMyPhTubeElements = async (phtubeCategoryId) => {
 
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="card w-72 h-96 bg-base-100 shadow-xl">
+        <div id="card-container" class="card w-72 h-96 bg-base-100 shadow-xl">
             <figure><img src=${phtubeElements?.thumbnail} alt="thumbnail-image" /></figure>
             <p class="text-right -my-4 bg-[#171717] text-white p-2">${timeInHoursMinutes}</p>
             <div class="card-body">
@@ -74,11 +73,80 @@ const handleMyPhTubeElements = async (phtubeCategoryId) => {
         </div>
             
         `;
-        
+
         myPhtubeCardContainer.appendChild(div);
 
     });
+
+}
+
+const handlePhtubeSortByViews = async (phtubeCategoryId) => {
+
+    // console.log(phtubeCategoryId);
+
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${phtubeCategoryId}`);
+    const data = await response.json();
+
+    
+
+    const sortByViewsOfPhtube = data.data;
+    // console.log(sortByViewsOfPhtube);
+    sortByViewsOfPhtube.sort((a, b) => {
+        const viewsA = parseFloat(a.others.views)
+        const viewsB = parseFloat(b.others.views)
+        return viewsB - viewsA;
+    })
+    
+
+
+    const myPhtubeCardContainer = document.getElementById('myphtube-card-container');
+    myPhtubeCardContainer.innerHTML = "";
+
+    sortByViewsOfPhtube.forEach((sortedViews) => {
+        // console.log(sortedViews.others.views);
+
+        // console.log(phtubeElements.authors[0].verified);
+        const verificationBlueTickMark = sortedViews.authors[0].verified;
+
+        // console.log(phtubeElements.others.posted_date);
+        let postDateSeconds = sortedViews.others.posted_date;
+        let postDateHours = Math.floor(postDateSeconds / 3600);
+        let postDateMinutes = Math.floor((postDateSeconds % 3600) / 60);
+        let timeInHoursMinutes = `${postDateHours > 0 ? postDateHours + ' ' + 'hrs' : ''} ${postDateMinutes > 0 ? postDateMinutes + ' ' + 'min' + ' ' + 'ago' : ''}`;
+        // console.log(timeInHoursMinutes);
+
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div id="card-container" class="card w-72 h-96 bg-base-100 shadow-xl">
+                <figure><img src=${sortedViews?.thumbnail} alt="thumbnail-image" /></figure>
+                <p class="text-right -my-4 bg-[#171717] text-white p-2">${timeInHoursMinutes}</p>
+                <div class="card-body">
+    
+                    <div class="flex flex-row">
+                        <img class="w-1/3 rounded-full mr-2" src=${sortedViews?.authors[0]?.profile_picture} alt="author-image">
+                        <h2 class="card-title">${sortedViews?.title}</h2>
+                    </div>
+    
+                    <div class="flex flex-row">
+                        <p>${sortedViews?.authors[0]?.profile_name}</p>
+                        <div>
+                        ${verificationBlueTickMark === true ? '<img src="./images/bluetick.svg" alt="blue-tick"/>' : ''}
+                        </div>
+                    </div>
+    
+                    <p>${sortedViews?.others?.views} views</p>
+                
+                </div>
+            </div>
+                
+            `;
+
+        myPhtubeCardContainer.appendChild(div);
+
+    });
+
 }
 
 handleMyPhTubeCategories();
 handleMyPhTubeElements("1000");
+handlePhtubeSortByViews();
